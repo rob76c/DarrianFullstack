6e143f6209ef2cc78223a787691d22980375489f
Robbie <rob76c@gmail.com>
2024-11-19 15:54:09 -0500

    json parsin middleware

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -1,13 +1,19 @@
 import { Router } from "express";
-import { listProducts, getProductbyID,createProduct,updateProduct,deleteProduct } from "./productsController";
+import {
+  listProducts,
+  getProductbyID,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "./productsController";
 
 //Products endpoints
-const router= Router();
+const router = Router();
 
-router.get('/', listProducts);
-router.get('/:id', getProductbyID);
-router.post('/', createProduct);
-router.put('/:id', updateProduct)
-router.delete('/:id', deleteProduct)
+router.get("/", listProducts);
+router.get("/:id", getProductbyID);
+router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 export default router;
--- api/src/routes/products/productsController.ts
+++ api/src/routes/products/productsController.ts
@@ -9,6 +9,7 @@ export function getProductbyID(req: Request, res:Response) {
 };
 
 export function createProduct(req: Request, res:Response) {
+    console.log(req.body);
     res.send('createProduct');
 };
 
