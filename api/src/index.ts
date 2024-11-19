aa5a4557ee57420e9620dbadaf687454132c7c73
Robbie <rob76c@gmail.com>
2024-11-19 17:19:32 -0500

    Drizzle setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,6 +4,8 @@ import productsRoutes from './routes/products/index'
 const port= 3000;
 
 const app = express();
+
+app.use(urlencoded({extended: false}));
 app.use(json());
 
 app.get('/', (req, res) => {
