c88aab7984113f55645055c0b7ecdd1c07c65a07
Robbie <rob76c@gmail.com>
2024-11-22 21:51:44 -0500

    Setup expo router

---
+++ ecommerce-mobile/app/cart.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function CartScreen(){
+    return(
+        <View>
+            <Text style= {{fontSize: 20}}>
+                Cart Screen
+            </Text>
+        </View>
+    )
+}
