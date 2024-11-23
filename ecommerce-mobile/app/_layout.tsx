3affb1bdfaee6fb3cb1bb7879d5b94b6a3dee70b
Robbie <rob76c@gmail.com>
2024-11-22 22:31:32 -0500

    Installed gluestack

--- ecommerce-mobile/app/_layout.tsx
+++ ecommerce-mobile/app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack} from "expo-router";
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+
 
 export default function RootLayout() {
-    return <Stack/>;
+    return(
+<GluestackUIProvider>
+<Stack/>
+</GluestackUIProvider>
+    );
 }
