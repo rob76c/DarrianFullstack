5c03d5d6f2535d240a2118d0b4aaf5df3d91b415
Robbie <rob76c@gmail.com>
2024-11-24 16:59:24 -0500

    Product ui using gluestacks

--- ecommerce-mobile/app/index.tsx
+++ ecommerce-mobile/app/index.tsx
@@ -1,12 +1,18 @@
 import { View, Text, FlatList } from "react-native";
 import products from "../assets/products.json";
 import ProductListItem from "../components/ProductListItem";
+import { Button, ButtonText } from "@/components/ui/button";
 
 export default function HomeScreen() {
   return (
     <FlatList
       data={products}
-      renderItem={({ item }) => <ProductListItem product={item} /> }
+      numColumns={2}
+      //Add spacing between rows
+      contentContainerClassName="gap-2"
+      //Add spacing between items
+      columnWrapperClassName="gap-2"
+      renderItem={({ item }) => <ProductListItem product={item} />}
     />
   );
 }
